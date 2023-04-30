75129209aaeec4575f40b6e54f6b882495f089f4
Chad Glazier <chadglazier@outlook.com>
2023-04-29 19:54:58 -0700

    fixed small bug preventing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Hey I'm Here</h1>
+        <h1>Hey I&apos;m Here</h1>
       </main>
     </>
   )
