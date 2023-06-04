d07c07de9b49a9da1de6c65b252a62b401b0f765
Chad-Glazier <chadglazier@outlook.com>
2023-06-04 16:18:43 -0700

    updated the meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Chad Glazier is a Computer Science student and a full-stack web developer." />
+        <meta name="description" content="Chad Glazier is a Computer Science undergrad student and full-stack web developer." />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
