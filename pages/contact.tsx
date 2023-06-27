704970deba336ce7bc6d15162dcd7e1c076974e0
Chad-Glazier <chadglazier@outlook.com>
2023-06-27 01:33:45 -0700

    updated minor details; buttons and stuff

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,60 +16,64 @@ export default function Contact() {
         width={300}
         alt="A picture of me!"
       />
-      <section
-        className={styles.socials}
+      <div
+        className={styles.info}
       >
-        <Link
-          href="https://www.linkedin.com/in/chad-glazier-4b585a257?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BiTFTt1cyT7OFkyvs9OdR2g%3D%3D"
-          target="_blank"
+        <section
+          className={styles.socials}
         >
-          <Image
-            className={styles.linkedInLink}
-            src="/linkedin.png"
-            alt="Click here to visit my LinkedIn Page!"
-            width={30}
-            height={30}
-          />          
-        </Link>
-        <Link
-          href="https://github.com/Chad-Glazier"
-          target="_blank"
-        >
-          <Image
-            className={styles.githubLink}
-            src="/github.svg"
-            alt="Click here to visit my Github!"
-            width={30}
-            height={30}
-          />          
-        </Link>
-      </section>
-      <table className={styles.contactInfo}>
-        <tbody>
-          <tr>
-            <th>Email</th>
-            <td>
-              <Link href="mailto:chadglazier@outlook.com" target="_blank">
-                chadglazier@outlook.com
-              </Link>
-            </td>
-          </tr>
-          <tr>
-            <th>Phone</th>
-            <td>
-              (250) 241-4039
-            </td>
-          </tr>
-          <tr>
-            <th>Location</th>
-            <td>
-              <Link href="https://goo.gl/maps/839DwLLUU2yUMRAC8" target="_blank">
-                Kelowna, BC
-              </Link>
-            </td>
-          </tr>          
-        </tbody>
-      </table>
+          <Link
+            href="https://www.linkedin.com/in/chad-glazier-4b585a257?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BiTFTt1cyT7OFkyvs9OdR2g%3D%3D"
+            target="_blank"
+          >
+            <Image
+              className={styles.linkedInLink}
+              src="/linkedin.png"
+              alt="Click here to visit my LinkedIn Page!"
+              width={30}
+              height={30}
+            />          
+          </Link>
+          <Link
+            href="https://github.com/Chad-Glazier"
+            target="_blank"
+          >
+            <Image
+              className={styles.githubLink}
+              src="/github.svg"
+              alt="Click here to visit my Github!"
+              width={30}
+              height={30}
+            />          
+          </Link>
+        </section>
+        <table className={styles.contactInfo}>
+          <tbody>
+            <tr>
+              <th>Email</th>
+              <td>
+                <Link href="mailto:chadglazier@outlook.com" target="_blank">
+                  chadglazier@outlook.com
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <th>Phone</th>
+              <td>
+                (250) 241-4039
+              </td>
+            </tr>
+            <tr>
+              <th>Location</th>
+              <td>
+                <Link href="https://goo.gl/maps/839DwLLUU2yUMRAC8" target="_blank">
+                  Kelowna, BC
+                </Link>
+              </td>
+            </tr>          
+          </tbody>
+        </table>
+      </div>
     </article>
   )
 }
