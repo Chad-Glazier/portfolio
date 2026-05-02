6ca42d741a835942ab56037d3cc524a2ba22a4a3
Chad Glazier <chadglazier@outlook.com>
2026-05-02 00:44:16 -0700

    tweaks

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,41 +17,33 @@ type NavProps = {
 function Nav({ onSelect }: NavProps) {
 
 	const [activeItem, setActiveItem] = useState<NavItem>("Introduction")
-	const [backgroundOffset, setBackgroundOffset] = useState("0% -25%")
 
 	useEffect(() => {
 		onSelect(activeItem)
 	}, [activeItem])
 
 	return <nav className={styles.nav}>
-		{navItems.map((name, idx) => <NavItem 
+		{navItems.map((name) => <NavItem 
 				activeItem={activeItem}
 				name={name}
 				onClick={() => setActiveItem(name)}
-				onMouseOver={() => {
-					setBackgroundOffset(`0% ${-idx * 10}%`)
-				}}
 				key={name}
 		/>)}
-		<div 
-			style={{ backgroundPosition: backgroundOffset}}
-			className={styles.backgroundPattern}></div>
+		<div className={styles.backgroundPattern}></div>
 	</nav>
 }
 
 type NavItemProps = { 
 	activeItem: NavItem | null,
 	onClick?: () => void 
-	onMouseOver?: () => void
 	name: NavItem
 }
 
-function NavItem({ activeItem, onClick, onMouseOver, name }: NavItemProps) {
+function NavItem({ activeItem, onClick, name }: NavItemProps) {
 	return <div
 		className={styles.item + 
 			(activeItem == name ? " " + styles.active : "")}
 		onClick={onClick}
-		onMouseOver={onMouseOver}
 	>
 		{name}
 	</div>
